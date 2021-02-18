1df571fb847a6fd6297c36e75470fa5c016f73df
medouassim <55862605+medouassim@users.noreply.github.com>
2021-02-18 01:28:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var container = document.querySelector('.container');
+var left = document.querySelector('.left');
+var right = document.querySelector('.right');
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'));
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'));
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'));
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'));
